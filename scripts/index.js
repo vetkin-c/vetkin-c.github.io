85532b0ddd9be3f1d4325c9bab84ed33cc8691e8
unknown <daffa.sweet100@gmail.com>
2024-10-05 16:39:46 +0700

    jaraaa

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// login page scripts
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  //validasi username dan password, username dan password bisa diganti sesuai keinginan
+  if (username === "Zahra" && password === "user") {
+    Swal.fire({
+      icon: "success",
+      title: "Login berhasil!",
+      text: "Selamat datang Zahra",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(function () {
+      window.location.href = "birthday.html";
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Login gagal!",
+      text: "Bukan buat lu berarti!",
+      confirmButtonText: "Coba lagi",
+      confirmButtonColor: "#ff7675",
+    });
+  }
+}
